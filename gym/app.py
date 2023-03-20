7b21fc9d4df619057b01052d2efa675d85d2bbac
DeepeshAhuja <deepeshahuja2@gmail.com>
2023-03-20 23:17:01 +0530

    commit

--- gym/app.py
+++ gym/app.py
@@ -5,11 +5,11 @@ from flask_session import Session
 from flask_mail import Mail, Message
 
 app= Flask(__name__)
-app.secret_key = 'xyz123abc'
-app.config['MYSQL_HOST']='localhost'
-app.config['MYSQL_USER']='root'
-app.config['MYSQL_PASSWORD']='root123$'
-app.config['MYSQL_DB']=''
+# app.secret_key = 'xyz123abc'
+# app.config['MYSQL_HOST']='localhost'
+# app.config['MYSQL_USER']='root'
+# app.config['MYSQL_PASSWORD']='root123$'
+# app.config['MYSQL_DB']=''
 
 mysql=MySQL(app)
 
@@ -17,4 +17,25 @@ mysql=MySQL(app)
 def index():
     return render_template("index.html")
 
-if __in
+@app.route('/profile')
+def profile():
+    return render_template("profile.html")
+
+@app.route('/membership')
+def membership():
+    return render_template("membership.html")
+
+@app.route('/attendance')
+def profile():
+    return render_template("attendance.html")
+
+@app.route('/login')
+def profile():
+    return render_template("login.html")
+
+@app.route('/signup')
+def profile():
+    return render_template("signup.html")
+
+if __name__=="__main__":
+    app.run(debug=True,port=8000)
